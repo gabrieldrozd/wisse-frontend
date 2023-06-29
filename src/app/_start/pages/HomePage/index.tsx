e4d6f8587efd50807d17501687284f842eeab782
Gabriel Drozd <drozdgabriel@pm.me>
2023-06-29 23:03:21 +0200

    #X | Removed unnecessary commented code from HomePage

--- src/app/_start/pages/HomePage/index.tsx
+++ src/app/_start/pages/HomePage/index.tsx
@@ -1,7 +1,6 @@
-import {Button as NextButton} from "@nextui-org/react";
-import {Box, Container} from "@mantine/core";
-import {HeroWelcome} from "@app.start/pages/HomePage/components/HeroWelcome";
 import {FeaturesPreview} from "@app.start/pages/HomePage/components/FeaturesPreview";
+import {HeroWelcome} from "@app.start/pages/HomePage/components/HeroWelcome";
+import {Box, Container} from "@mantine/core";
 
 export const HomePage = () => {
     return (
@@ -13,61 +12,6 @@ export const HomePage = () => {
             <Box mt={40}>
                 <FeaturesPreview />
             </Box>
-
-            {/*<Group m={0}>*/}
-            {/*    <NextButton bordered size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton shadow bordered size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton ghost animated size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton animated size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton shadow animated size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton flat animated size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton flat shadow animated size="sm" color="primary">primary</NextButton>*/}
-            {/*    <NextButton light animated size="sm" color="primary">primary</NextButton>*/}
-            {/*</Group>*/}
-
-            {/*<Group m={0}>*/}
-            {/*    <NextButton bordered size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton shadow bordered size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton ghost animated size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton animated size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton shadow animated size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton flat animated size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton flat shadow animated size="sm" color="secondary">secondary</NextButton>*/}
-            {/*    <NextButton light animated size="sm" color="secondary">secondary</NextButton>*/}
-            {/*</Group>*/}
-
-            {/*<Group m={0}>*/}
-            {/*    <NextButton bordered size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton shadow bordered size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton ghost animated size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton animated size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton shadow animated size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton flat animated size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton flat shadow animated size="sm" color="success">success</NextButton>*/}
-            {/*    <NextButton light animated size="sm" color="success">success</NextButton>*/}
-            {/*</Group>*/}
-
-            {/*<Group m={0}>*/}
-            {/*    <NextButton bordered size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton shadow bordered size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton ghost animated size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton animated size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton shadow animated size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton flat animated size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton flat shadow animated size="sm" color="warning">warning</NextButton>*/}
-            {/*    <NextButton light animated size="sm" color="warning">warning</NextButton>*/}
-            {/*</Group>*/}
-
-            {/*<Group m={0}>*/}
-            {/*    <NextButton bordered size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton shadow bordered size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton ghost animated size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton animated size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton shadow animated size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton flat animated size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton flat shadow animated size="sm" color="error">error</NextButton>*/}
-            {/*    <NextButton light animated size="sm" color="error">error</NextButton>*/}
-            {/*</Group>*/}
         </Container>
     );
 };
